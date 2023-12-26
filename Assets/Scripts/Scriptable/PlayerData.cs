94d878397bf49be5d974039cf2ebe7ccdfe39df7
ArKloK <ccamachobanez@gmail.com>
2023-12-26 11:45:32 +0100

    Side movement and jumping added to PlayerMovementController

--- Assets/Scripts/Scriptable/PlayerData.cs
+++ Assets/Scripts/Scriptable/PlayerData.cs
@@ -1,103 +1,56 @@
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Player Data With Dash")] //Create a new playerData object by right clicking in the Project Menu then Create/Player/Player Data and drag onto the player
-public class PlayerDataWithDash : ScriptableObject
+namespace TarodevController
 {
-	[Header("Gravity")]
-	[HideInInspector] public float gravityStrength; //Downwards force (gravity) needed for the desired jumpHeight and jumpTimeToApex.
-	[HideInInspector] public float gravityScale; //Strength of the player's gravity as a multiplier of gravity (set in ProjectSettings/Physics2D).
-												 //Also the value the player's rigidbody2D.gravityScale is set to.
-	[Space(5)]
-	public float fallGravityMult; //Multiplier to the player's gravityScale when falling.
-	public float maxFallSpeed; //Maximum fall speed (terminal velocity) of the player when falling.
-	[Space(5)]
-	public float fastFallGravityMult; //Larger multiplier to the player's gravityScale when they are falling and a downwards input is pressed.
-									  //Seen in games such as Celeste, lets the player fall extra fast if they wish.
-	public float maxFastFallSpeed; //Maximum fall speed(terminal velocity) of the player when performing a faster fall.
+    [CreateAssetMenu]
+    public class ScriptableStats : ScriptableObject
+    {
+        [Header("LAYERS")] [Tooltip("Set this to the layer your player is on")]
+        public LayerMask PlayerLayer;
 
-	[Space(20)]
+        [Header("INPUT")] [Tooltip("Makes all Input snap to an integer. Prevents gamepads from walking slowly. Recommended value is true to ensure gamepad/keybaord parity.")]
+        public bool SnapInput = true;
 
-	[Header("Run")]
-	public float runMaxSpeed; //Target speed we want the player to reach.
-	public float runAcceleration; //The speed at which our player accelerates to max speed, can be set to runMaxSpeed for instant acceleration down to 0 for none at all
-	[HideInInspector] public float runAccelAmount; //The actual force (multiplied with speedDiff) applied to the player.
-	public float runDecceleration; //The speed at which our player decelerates from their current speed, can be set to runMaxSpeed for instant deceleration down to 0 for none at all
-	[HideInInspector] public float runDeccelAmount; //Actual force (multiplied with speedDiff) applied to the player .
-	[Space(5)]
-	[Range(0f, 1)] public float accelInAir; //Multipliers applied to acceleration rate when airborne.
-	[Range(0f, 1)] public float deccelInAir;
-	[Space(5)]
-	public bool doConserveMomentum = true;
+        [Tooltip("Minimum input required before you mount a ladder or climb a ledge. Avoids unwanted climbing using controllers"), Range(0.01f, 0.99f)]
+        public float VerticalDeadZoneThreshold = 0.3f;
 
-	[Space(20)]
+        [Tooltip("Minimum input required before a left or right is recognized. Avoids drifting with sticky controllers"), Range(0.01f, 0.99f)]
+        public float HorizontalDeadZoneThreshold = 0.1f;
 
-	[Header("Jump")]
-	public float jumpHeight; //Height of the player's jump
-	public float jumpTimeToApex; //Time between applying the jump force and reaching the desired jump height. These values also control the player's gravity and jump force.
-	[HideInInspector] public float jumpForce; //The actual force applied (upwards) to the player when they jump.
+        [Header("MOVEMENT")] [Tooltip("The top horizontal movement speed")]
+        public float MaxSpeed = 14;
 
-	[Header("Both Jumps")]
-	public float jumpCutGravityMult; //Multiplier to increase gravity if the player releases the jump button while still jumping
-	[Range(0f, 1)] public float jumpHangGravityMult; //Reduces gravity while close to the apex (desired max height) of the jump
-	public float jumpHangTimeThreshold; //Speeds (close to 0) where the player will experience extra "jump hang". The player's velocity.y is closest to 0 at the jump's apex (think of the gradient of a parabola or quadratic function)
-	[Space(0.5f)]
-	public float jumpHangAccelerationMult;
-	public float jumpHangMaxSpeedMult;
+        [Tooltip("The player's capacity to gain horizontal speed")]
+        public float Acceleration = 120;
 
-	[Header("Wall Jump")]
-	public Vector2 wallJumpForce; //The actual force (this time set by us) applied to the player when wall jumping.
-	[Space(5)]
-	[Range(0f, 1f)] public float wallJumpRunLerp; //Reduces the effect of player's movement while wall jumping.
-	[Range(0f, 1.5f)] public float wallJumpTime; //Time after wall jumping the player's movement is slowed for.
-	public bool doTurnOnWallJump; //Player will rotate to face wall jumping direction
+        [Tooltip("The pace at which the player comes to a stop")]
+        public float GroundDeceleration = 60;
 
-	[Space(20)]
+        [Tooltip("Deceleration in air only after stopping input mid-air")]
+        public float AirDeceleration = 30;
 
-	[Header("Slide")]
-	public float slideSpeed;
-	public float slideAccel;
+        [Tooltip("A constant downward force applied while grounded. Helps on slopes"), Range(0f, -10f)]
+        public float GroundingForce = -1.5f;
 
-	[Header("Assists")]
-	[Range(0.01f, 0.5f)] public float coyoteTime; //Grace period after falling off a platform, where you can still jump
-	[Range(0.01f, 0.5f)] public float jumpInputBufferTime; //Grace period after pressing jump where a jump will be automatically performed once the requirements (eg. being grounded) are met.
+        [Tooltip("The detection distance for grounding and roof detection"), Range(0f, 2.7f)]
+        public float GrounderDistance = 2.7f;
 
-	[Space(20)]
+        [Header("JUMP")] [Tooltip("The immediate velocity applied when jumping")]
+        public float JumpPower = 36;
 
-	[Header("Dash")]
-	public int dashAmount;
-	public float dashSpeed;
-	public float dashSleepTime; //Duration for which the game freezes when we press dash but before we read directional input and apply a force
-	[Space(5)]
-	public float dashAttackTime;
-	[Space(5)]
-	public float dashEndTime; //Time after you finish the inital drag phase, smoothing the transition back to idle (or any standard state)
-	public Vector2 dashEndSpeed; //Slows down player, makes dash feel more responsive (used in Celeste)
-	[Range(0f, 1f)] public float dashEndRunLerp; //Slows the affect of player movement while dashing
-	[Space(5)]
-	public float dashRefillTime;
-	[Space(5)]
-	[Range(0.01f, 0.5f)] public float dashInputBufferTime;
+        [Tooltip("The maximum vertical movement speed")]
+        public float MaxFallSpeed = 40;
 
+        [Tooltip("The player's capacity to gain fall speed. a.k.a. In Air Gravity")]
+        public float FallAcceleration = 110;
 
-	//Unity Callback, called when the inspector updates
-	private void OnValidate()
-	{
-		//Calculate gravity strength using the formula (gravity = 2 * jumpHeight / timeToJumpApex^2) 
-		gravityStrength = -(2 * jumpHeight) / (jumpTimeToApex * jumpTimeToApex);
+        [Tooltip("The gravity multiplier added when jump is released early")]
+        public float JumpEndEarlyGravityModifier = 3;
 
-		//Calculate the rigidbody's gravity scale (ie: gravity strength relative to unity's gravity value, see project settings/Physics2D)
-		gravityScale = gravityStrength / Physics2D.gravity.y;
+        [Tooltip("The time before coyote jump becomes unusable. Coyote jump allows jump to execute even after leaving a ledge")]
+        public float CoyoteTime = .15f;
 
-		//Calculate are run acceleration & deceleration forces using formula: amount = ((1 / Time.fixedDeltaTime) * acceleration) / runMaxSpeed
-		runAccelAmount = (50 * runAcceleration) / runMaxSpeed;
-		runDeccelAmount = (50 * runDecceleration) / runMaxSpeed;
-
-		//Calculate jumpForce using the formula (initialJumpVelocity = gravity * timeToJumpApex)
-		jumpForce = Mathf.Abs(gravityStrength) * jumpTimeToApex;
-
-		#region Variable Ranges
-		runAcceleration = Mathf.Clamp(runAcceleration, 0.01f, runMaxSpeed);
-		runDecceleration = Mathf.Clamp(runDecceleration, 0.01f, runMaxSpeed);
-		#endregion
-	}
+        [Tooltip("The amount of time we buffer a jump. This allows jump input before actually hitting the ground")]
+        public float JumpBuffer = .2f;
+    }
 }
