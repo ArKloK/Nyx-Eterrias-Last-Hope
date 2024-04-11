50e96f873adff838c0034556b92e5d8eaabf8134
ArKloK <ccamachobanez@gmail.com>
2024-04-11 12:57:19 +0200

    Audio, Music and Brightness settings

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -1,17 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Audio;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] AudioMixer audioMixer;
+    [SerializeField] Image darkOverlay;
+
+    [Header("Sliders")]
+    [SerializeField] Slider brightnessSlider;
+    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider sfxSlider;
+
     [Header("Buttons")]
     [SerializeField] private Button newGameButton;
     [SerializeField] private Button loadGameButton;
 
     void Start()
     {
+        LoadVolume();
+        LoadDarkOverlay();
         DontDestroyOnLoadScript.DestroyAll();
         if(DataPersistenceManager.Instance.HasGameData())
         {
@@ -22,25 +32,70 @@ public class MainMenu : MonoBehaviour
             loadGameButton.gameObject.SetActive(false);
         }
     }
-
+    void Update()
+    {
+        ChangeDarkOverlay();
+    }
     public void OnNewGameButtonClicked()
     {
         DisableMenuButtons();
         DataPersistenceManager.Instance.CreateNewGame = true;
         LevelManager.instance.LoadScene("FirstLevel", "CrossFade");
     }
-
     public void OnLoadGameButtonClicked()
     {
         DisableMenuButtons();
         SceneManager.LoadSceneAsync(PlayerPrefs.GetInt("currentSceneIndex", 1));
     }
-
     public void OnQuitGameButtonClicked()
     {
         Application.Quit();
     }
+    public void UpdateMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+    public void UpdateSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", volume);
+    }
+    public void SaveVolume()
+    {
+        audioMixer.GetFloat("MusicVolume", out float musicVolume);
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
 
+        audioMixer.GetFloat("SFXVolume", out float sfxVolume);
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+    }
+    public void LoadVolume()
+    {
+        if(PlayerPrefs.HasKey("MusicVolume"))
+        {
+            musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        }
+        if(PlayerPrefs.HasKey("SFXVolume"))
+        {
+            sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+        }
+    
+    }
+    private void ChangeDarkOverlay()
+    {
+        var tempColor = darkOverlay.color;
+        tempColor.a = brightnessSlider.value;
+        darkOverlay.color = tempColor;
+    }
+    public void LoadDarkOverlay()
+    {
+        if(PlayerPrefs.HasKey("DarkOverlay"))
+        {
+            brightnessSlider.value = PlayerPrefs.GetFloat("DarkOverlay");
+        }
+    }
+    public void SaveDarkOverlay()
+    {
+        PlayerPrefs.SetFloat("DarkOverlay", brightnessSlider.value);
+    }
     private void DisableMenuButtons()
     {
         newGameButton.interactable = false;
--- Assets/Scripts/UI/PauseMenuController.cs
+++ Assets/Scripts/UI/PauseMenuController.cs
@@ -1,25 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenuController : MonoBehaviour
 {
     private PlayerMovementController.PlayerMovementController playerMovementController;
-    public static bool isPaused = false;
-    public GameObject pauseMenuUI;
-    public GameObject inventoryUI;
-    public Inventory inventory;
+    [SerializeField] static bool isPaused = false;
+    [SerializeField] GameObject pauseMenuUI;
+    [SerializeField] GameObject inventoryUI;
+    [SerializeField] Inventory inventory;
+    [Header("Settings")]
+    [SerializeField] AudioMixer audioMixer;
+    [SerializeField] Image darkOverlay;
+    [SerializeField] Slider brightnessSlider;
+    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider sfxSlider;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        LoadVolume();
+        LoadDarkOverlay();
         playerMovementController = FindObjectOfType<PlayerMovementController.PlayerMovementController>();
     }
 
     // Update is called once per frame
     public void HandleUpdate()
     {
+        ChangeDarkOverlay();
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (inventoryUI.activeSelf)
@@ -60,5 +72,50 @@ public class PauseMenuController : MonoBehaviour
     public void BackToMainMenu()
     {
         SceneManager.LoadSceneAsync("Main Menu");
+    }
+    public void UpdateMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+    public void UpdateSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", volume);
+    }
+    public void SaveVolume()
+    {
+        audioMixer.GetFloat("MusicVolume", out float musicVolume);
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+
+        audioMixer.GetFloat("SFXVolume", out float sfxVolume);
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+    }
+    public void LoadVolume()
+    {
+        if (PlayerPrefs.HasKey("MusicVolume"))
+        {
+            musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        }
+        if (PlayerPrefs.HasKey("SFXVolume"))
+        {
+            sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+        }
+
+    }
+    private void ChangeDarkOverlay()
+    {
+        var tempColor = darkOverlay.color;
+        tempColor.a = brightnessSlider.value;
+        darkOverlay.color = tempColor;
+    }
+    public void LoadDarkOverlay()
+    {
+        if (PlayerPrefs.HasKey("DarkOverlay"))
+        {
+            brightnessSlider.value = PlayerPrefs.GetFloat("DarkOverlay");
+        }
+    }
+    public void SaveDarkOverlay()
+    {
+        PlayerPrefs.SetFloat("DarkOverlay", brightnessSlider.value);
     }
 }
